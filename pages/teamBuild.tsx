7d821c2958bfd17ba045a1a65b7fcfd274cd2f49
travis <trvsmartin@gmail.com>
2023-05-26 09:08:57 -0700

    feat: save up to 6 pokemon on team in team builder!

--- pages/teamBuild.tsx
+++ pages/teamBuild.tsx
@@ -4,6 +4,7 @@ import {
   NativeSyntheticEvent,
   TextInputChangeEventData,
   ScrollView,
+  Alert,
 } from 'react-native';
 //theme styling
 import ThemeContext from '../theme';
@@ -17,7 +18,7 @@ import PokDictionary from '../components/pokDictionary/pokDictionary';
 
 const TeamBuilder = (): JSX.Element => {
   //state for team move TODO: move to app level, create context or pass listeners
-  const [team, setTeam] = useState({});
+  const [team, setTeam] = useState<Object[]>([]);
   //state for search term by pokemon name
   const [pokSearch, setPokSearch] = useState<string>('');
   const {theme} = useContext(ThemeContext);
@@ -27,21 +28,33 @@ const TeamBuilder = (): JSX.Element => {
   };
   const clickHandler = (item, arr) => {
     //TODO: update so this spreads active item into team
-    setTeam(activeItem(item, arr));
+    if (team.length < 6) {
+      setTeam([...team, activeItem(item, arr)]);
+    } else if (team.length === 6) {
+      Alert.alert(
+        'Team Full',
+        'You already have six Pokemon on your team, no room for more',
+        [{text: 'OK'}],
+      );
+    }
   };
 
   return (
     <>
       <Header title="Team Builder" />
-      <Header
-        title={Object.keys(team).length ? team.name : 'Display team member'}
-      />
-      {/* six slots */}
-      {/* search function by pokemon name */}
       <ScrollView
         style={{
           backgroundColor: theme.colors.primary,
         }}>
+      {team.length ? (
+        team.map(member => {
+          return <Header title={member.name} />;
+        })
+      ) : (
+        <Text>Add team members to display</Text>
+      )}
+      {/* six slots */}
+      {/* search function by pokemon name */}
         <Search
           placeholder="Search for a Pokemon by name"
           changeHandler={changeHandler}
